b626545784257245630840d4a5418c944cb5d603
Emmanuel Lara <ea.lara21@info.uas.edu.mx>
2023-08-03 17:09:11 -0700

    Comentario
    Prueba

--- Assets/Scrips/PlayerController 2.cs
+++ Assets/Scrips/PlayerController 2.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Comentario de prueba
 public class PlayerController2 : MonoBehaviour
 {
     private Rigidbody2D Rigidbody2D;
